d3d8f48ec61480240fccf7f52bce4d6e31ac173d
TejuPhasale04 <119858988+TejuPhasale04@users.noreply.github.com>
2024-11-15 11:09:13 +0530

    initial comment

--- src/components/Featuresection.jsx
+++ src/components/Featuresection.jsx
@@ -1,32 +1,33 @@
-import React from 'react';
-import { service } from '../constants';
+import React from 'react'
+import { features } from '../constants'
 
 const Featuresection = () => {
-    return (
-      <section className="bg-gradient-to-black from-gray-800 via-gray-900 to-black py-16">
-        <div className="container mx-auto px-4">
-          <h2 className="text-4xl font-extrabold text-gray-200 text-center mb-12">Our Services</h2>
-          <div className="grid lg:gap-10 gap-1 grid-cols-1 lg:grid-cols-3">
-            {service.map((feature, index) => (
-              <div
-                key={index}
-                className="relative group bg-gray-800 bg-opacity-30 hover:bg-opacity-50 rounded-lg p-8 text-center shadow-lg hover:shadow-2xl transition-all duration-500 transform hover:-translate-y-2"
-              >
-                <div className="text-xl lg:text-6xl mb-5 text-gray-400 group-hover:text-gray-100 transition-colors duration-300">
-                  {feature.icon}
+  return (
+   <div className="mt-10 border-b border-neutral-800 relative min-h-[800px]">
+    <div className="text-center">
+        <span className='bg-neutral-900 text-orange-500 rounded-full
+        h-6 text-sm font-medium px-2 py-1 uppercase'>Feature</span>
+        <h2 className='mt-10 lg:mt-20 text-3xl sm:text-5xl lg:text-6xl'>Easily build&nbsp;
+            <span className='bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text'>your code</span></h2>
+    </div>
+    <div className="flex flex-wrap mt-10 lg:mt-20">
+        {features.map((feature,key)=>(
+            <div key={key} className="w-full sm:w-1/2 lg:w-1/3">
+            <div className="flex">
+                <div className="flex mx-6 justify-center items-center bg-neutral-900 rounded-full size-10 text-orange-700">
+                    {feature.icon}
                 </div>
-                <h5 className="lg:text-xl text-xs font-bold mb-3 text-gray-200 group-hover:text-white transition-colors">
-                  {feature.title}
-                </h5>
-                <p className="text-gray-400 text-[0.5rem]">{feature.description}</p>
-                
-                <div className="absolute inset-0 bg-gradient-to-br from-gray-700 to-gray-900 opacity-0 group-hover:opacity-30 transition-opacity duration-500 rounded-lg"></div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-    );
-};
+                <div>
+                <h5 className="mt-1 mb-6 text-xl">{feature.text}</h5>
+                <p className='text-md text-neutral-500 mb-20 p-2'>{feature.description}</p>
+                </div>
+            </div>
+            </div>
+        ))}
+    </div>
+   </div>
+ 
+  )
+}
 
-export default Featuresection;
+export default Featuresection
