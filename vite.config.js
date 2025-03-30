92a29f5f3582d04e26250cc6ff17a16a8715eb6b
yashwardhancodes <143013221+yashwardhancodes@users.noreply.github.com>
2025-03-30 16:45:04 +0530

    added some deleted some

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.PORT) || 5173, // Use Render's assigned port or default to 5173
+    host: '0.0.0.0', // Ensure it binds to external access
+  },
+  preview: {
+    port: 8080,
+    host: '0.0.0.0',
+  }
 })
